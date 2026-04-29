2f0816afd0c0cdfe0f0d58d31c142b0bb353aa51
Mercy Robert <76105163+mercyroberts@users.noreply.github.com>
2026-04-29 13:08:49 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+        const revStr = cleaned.split('').reverse().join('');
+        return cleaned === revStr;
+    }
+}
