2795b6ec86f8853b2e8908609f13f7b2fa549e4b
Mercy Robert <76105163+mercyroberts@users.noreply.github.com>
2026-04-30 20:54:23 +0100

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.js
@@ -0,0 +1,47 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    // isPalindrome(s) {
+    //     const cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    //     const revStr = cleaned.split('').reverse().join('');
+    //     return cleaned === revStr;
+    // }
+
+    isPalindrome(s){
+        let newStr = s.toLowerCase();
+        let left = 0;
+        let right = newStr.length - 1;
+
+        while(left < right){
+            while(left < right && !this.isAlphanumeric(newStr[left])) left++
+            while(right > left && !this.isAlphanumeric(newStr[right])) right--
+
+            if(newStr[left] !== newStr[right]) return false
+            left++
+            right--
+        }
+            return true
+    }
+    isAlphanumeric(newStrChar){
+        return (newStrChar >= 'a' && newStrChar <= 'z') || (newStrChar >= '0' && newStrChar <= '9')
+    }
+
+    // isPalindrome(s){
+    //     s = s.toLowerCase();
+    //     let left = 0;
+    //     let right = s.length - 1;
+
+    //     let isAlphanum = ((c) => (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'));
+        
+    //     while(left < right){
+    //         while(left < right && !isAlphanum(s[left])) left++;
+    //         while(left < right && !isAlphanum(s[right])) right--;
+    //         if(s[left] !== s[right]) return false;
+    //         left++;
+    //         right --
+    //     }
+    //     return true;
+    // }
+}
