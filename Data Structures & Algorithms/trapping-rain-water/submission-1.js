7644decbe384337ba82d4e5212b92585d3e107ba
Mercy Robert <76105163+mercyroberts@users.noreply.github.com>
2026-05-03 20:03:33 +0100

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    // trap(height) {
+    //     let total = 0;
+
+    //     for(let i = 0; i < height.length; i++){
+    //         let leftMax = 0;
+    //         for(let j = 0; j <= i; j++) leftMax = Math.max(leftMax, height[j])
+
+    //         let rightMax = 0;
+    //         for(let j = i; j < height.length; j++) rightMax = Math.max(rightMax, height[j])
+
+    //         total += Math.min(leftMax, rightMax) - height[i]
+    //     }
+    //     return total;
+    // }
+
+    trap(height){
+        let total = 0;
+        let left = 0;
+        let right = height.length - 1;
+        let leftMax = 0;
+        let rightMax = 0;
+        let water = 0;
+
+        while(left < right){
+            //[0,2,0,3,1,0,1,3,2,1]
+            if(height[left] < height[right]){
+                leftMax = Math.max(leftMax, height[left]);
+                water = leftMax - height[left];
+                total += water;
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                water = rightMax - height[right];
+                total += water
+                right--;
+            }
+        }
+        return total;
+    }
+}
