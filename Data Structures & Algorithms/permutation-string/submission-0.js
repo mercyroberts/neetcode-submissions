4146016aaf2ce9d5c5b45e6712c579f0d55d9f4c
Mercy Robert <76105163+mercyroberts@users.noreply.github.com>
2026-05-11 20:51:53 +0100

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        let s1Count = new Map();
+
+        const mapsEqual = (a, b) => {
+        if(a.length !== b.length) return false;
+
+        for(const [key, val] of a){
+            if (b.get(key) !== val) return false;
+        }
+        return true;
+        }
+
+        for(const c of s1){
+            s1Count.set(c, (s1Count.get(c) || 0) + 1);
+        }
+
+        let winCount  = new Map();
+
+        for(let i =0; i < s1.length; i++){
+            let c = s2[i];
+            winCount.set(c, (winCount.get(c) || 0 ) + 1 );
+        }
+
+        if(mapsEqual(s1Count, winCount)) return true;
+
+        for(let i = s1.length; i< s2.length; i++ ){
+            let enteringChar = s2[i];
+            let leavingChar = s2[i - s1.length];
+
+            winCount.set(enteringChar, (winCount.get(enteringChar) || 0 ) + 1);
+
+            let newCount = winCount.get(leavingChar) - 1;
+            if(newCount === 0){
+                winCount.delete(leavingChar);
+            }else {
+                winCount.set(leavingChar, newCount);
+            }
+
+            if (mapsEqual(s1Count, winCount)) return true;
+        }
+        return false
+
+    }
+}
