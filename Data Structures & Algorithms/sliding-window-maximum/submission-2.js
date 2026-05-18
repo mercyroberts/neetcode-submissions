f5f8bf75766097b7ebb6539868c65888ce1a794d
Mercy Robert <76105163+mercyroberts@users.noreply.github.com>
2026-05-18 22:49:51 +0100

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+//     maxSlidingWindow(nums, k) {
+//     const result = [];
+//     const deque = [];  // stores indices, values strictly decreasing
+    
+//     for (let right = 0; right < nums.length; right++) {
+//         // Step A: pop from back while back's value <= current value
+//         while (deque.length > 0 && nums[deque[deque.length - 1]] <= nums[right]) {
+//             deque.pop();
+//         }
+        
+//         // Step B: push current index
+//         deque.push(right);
+        
+//         // Step C: pop from front if it's outside the window
+//         if (deque[0] < right - k + 1) {
+//             deque.shift();
+//         }
+        
+//         // Step D: once window is fully formed, record max
+//         if (right >= k - 1) {
+//             result.push(nums[deque[0]]);
+//         }
+//     }
+    
+//     return result;
+// }
+
+        maxSlidingWindow(nums, k){
+            let result = [];
+            for(let i = 0; i <= nums.length - k; i++){
+                let max = -Infinity;
+                for(let j = i; j < i+k; j++){
+                    max = Math.max(max, nums[j]);
+                }
+                result.push(max)
+            }
+            return result;
+        }
+}
