8454b7cc9721ba4310866695daf7f5bfc95e7833
Mercy Robert <76105163+mercyroberts@users.noreply.github.com>
2026-05-19 14:52:30 +0100

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const match = {
+            ")" : '(',
+            "}" : '{',
+            "]" : '['
+        }
+
+        for(const c of s){
+            if(c === '(' || c === '{' || c === '['){
+                stack.push(c)
+            } else {
+                if(stack.length === 0) return false 
+                if(stack[stack.length - 1] !== match[c]) return false 
+                stack.pop(c);
+            }
+        }
+        return stack.length === 0;
+    }
+}
