a554ebeccd737551a1111658e177e8cb62ef4cc8
Mercy Robert <76105163+mercyroberts@users.noreply.github.com>
2026-05-21 19:11:57 +0100

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = [];
+        for(const token of tokens){
+            if(token === '+' || token === '-' || token === '*' || token === '/'){
+                const b = stack.pop();
+                const a = stack.pop();
+
+                let result;
+                if(token === '+') result = a + b
+                else if (token === '-') result = a - b
+                else if(token === '*') result = a * b
+                else result = Math.trunc(a/b)
+
+                stack.push(result);
+            } else {
+                stack.push(Number(token))
+            }
+        }
+        return stack[0];
+    }
+}
