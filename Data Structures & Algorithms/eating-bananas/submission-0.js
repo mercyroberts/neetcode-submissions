0c0e329dc4ab1817af6115a47bdf0d11ccb124ec
Mercy Robert <76105163+mercyroberts@users.noreply.github.com>
2026-06-04 17:40:30 +0100

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+         let left = 1;
+         let right = Math.max(... piles)
+         let answer = right;
+
+         while (left <= right){
+            let k = Math.floor((left + right) / 2)
+
+            let hour = 0;
+
+            for(const pile of piles){
+                hour += Math.ceil( pile/k)
+            }
+            
+            if(hour <= h) {
+                answer = k;
+                right = k - 1;
+            }else {
+                left = k + 1 
+            }
+         }
+         return answer
+    }
+}
