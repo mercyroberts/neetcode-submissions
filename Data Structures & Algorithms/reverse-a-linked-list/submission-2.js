610caa3863d4c7028e2e54940dcd41b869aae1cf
Mercy Robert <76105163+mercyroberts@users.noreply.github.com>
2026-06-29 12:29:31 +0100

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    // reverseList(head) {
+    //     let curr = head
+    //     let prev = null
+
+    //     while(curr !== null){
+    //         let next = curr.next
+    //         curr.next = prev
+    //         prev = curr
+    //         curr = next
+    //     }
+    //     return prev
+    // }
+
+    reverseList(head){
+        let prev = null
+        let curr = head
+
+        while(curr !== null){
+            let next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        return prev
+    }
+}
